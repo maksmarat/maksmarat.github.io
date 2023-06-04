4e6a754ce3f7263552b3b12afaad19e8f61314c1
maksmarat <maksmarat164@yandex.ru>
2023-06-05 04:04:59 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+        <Card width='200px' height='200px'>
+            <button>NIGER</button>
+        </Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface CardProps{
+    width?: string;
+    height?: string;
+    children: React.ReactNode;
+}
+
+function Card( props : CardProps) {
+    const name = "Nick";
+    const greeting = `Hello ${name}`; // "Hello Nick"
+    return (
+        <div style = {{backgroundColor: "lightblue", width: props.width, height: props.height, border: '5px solid black' }}>
+
+            <h1>Welcome {props.height} {props.width}</h1>
+            {props.children}
+
+        </div>
+    );
+}
+export default Card;
